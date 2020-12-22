dbff323ba04dca42be1a4c59fb3d2e87f9883cc1
Karris  Black <karris.b@gmail.com>
2020-12-22 22:24:04 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,6 @@
-# Write your code here
+magic_exit_number = 7 # Write your code here
+count = 0 
+while count < 10 && count ! = magic_exit_number do 
+  puts "Happy New Year"
+count = count + 1
+end
